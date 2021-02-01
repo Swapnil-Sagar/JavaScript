624cd0d51cc8e25f4d4bf5aaed68c9c21663096f
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2021-02-02 02:28:20 +0530

    Shortest Code

---
+++ ShortestCode.js
@@ -0,0 +1,4 @@
+//Even if the file is Empty Js will still create an execution context with window log.
+//JS engine creates this keyword and Window object
+
+// anythibg created in global space is attached to window
