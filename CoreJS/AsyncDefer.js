aef0a5cfb1c9666e88093d8706cfe7aa645f1e42
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2021-02-09 13:52:06 +0530

    Async and Defer

---
+++ CoreJS/AsyncDefer.js
@@ -0,0 +1,17 @@
+//Normal Script
+
+<script src="https://javascript.info/article/script-async-defer/long.js?speed=1"></script>;
+
+//Async
+
+<script
+  async
+  src="https://javascript.info/article/script-async-defer/long.js"
+></script>;
+
+//Defer
+
+<script
+  defer
+  src="https://javascript.info/article/script-async-defer/long.js"
+></script>;
